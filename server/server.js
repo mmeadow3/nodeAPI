243705e05cc6ca36526f7e1ecbfbaa076aa68787
Mike Meadows <mmeadow33@gmail.com>
2016-09-26 11:57:58 -0500

    added db interactions

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const express = require('express');
+const mongoose = require('mongoose');
 
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/meanChat"
 const app = express()
 
 const port = process.env.PORT || 6060
@@ -12,22 +14,25 @@ app.use(express.static('../client')) ////////telling to host files from the clie
 app.get('/api/title', (req, res) =>
   res.send({title: "Hello from the mean MEAN stack"})
 )
-app.get('/api/messages', (req, res) =>
-  res.json({
-    messages: [
-      {
-        author: 'John',
-        content: 'Get a Job!',
-      },
-      {
-        author: 'Scott',
-        content: 'Node is Awesome!',
-      },
-    ],
-  })
+
+const Message = mongoose.model("message", {
+  author: String,
+  content: String
+})
+
+app.get('/api/messages', (req, res, err) =>
+  Message
+  .find()
+  .then(messages =>
+    res.json({messages})
+  )
+  .catch(err)
 )
 
+mongoose.Promise = Promise
 
+mongoose.connect(MONGODB_URL, () =>
 app.listen(port, () =>
   console.log(`listening on port ${port}`)
+  )
 )
