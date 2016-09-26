2630bfe089e21aae8403eb01f6b37312c7fd0d06
Mike Meadows <mmeadow33@gmail.com>
2016-09-26 13:45:30 -0500

    done with day1, more updates tomorrow

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const {json}= require('body-parser');
 
 const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/meanChat"
 const app = express()
@@ -10,29 +11,32 @@ const port = process.env.PORT || 6060
 // app.set("port", port)  // technically do not need
 /////////middle wares////////////
 app.use(express.static('../client')) ////////telling to host files from the client folder
+app.use(json())
 /////////////////////////////////////
 app.get('/api/title', (req, res) =>
-  res.send({title: "Hello from the mean MEAN stack"})
+  res.json({ title: 'MEAN Chat' })
 )
 
-const Message = mongoose.model("message", {
+const Message = mongoose.model('message', {
   author: String,
-  content: String
+  content: String,
 })
 
 app.get('/api/messages', (req, res, err) =>
   Message
-  .find()
-  .then(messages =>
-    res.json({messages})
-  )
-  .catch(err)
+    .find()
+    .then(messages => res.json({ messages }))
+    .catch(err)
+)
+
+app.post('/api/messages', (req, res, err) =>
+  Message
+    .create(req.body)
+    .then(msg => res.json(msg))
+    .catch(err)
 )
 
 mongoose.Promise = Promise
-
 mongoose.connect(MONGODB_URL, () =>
-app.listen(port, () =>
-  console.log(`listening on port ${port}`)
-  )
+  app.listen(port, () => console.log(`Listening on port: ${port}`))
 )
